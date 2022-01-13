6cc6971e348eb5e49b67754d1b55df72ae5b10ba
EugeniaAlex <eugenia.aleksandrova@gmail.com>
2022-01-13 23:22:59 +0200

    task#1
    Добавляет решение задачи №1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,50 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divGaleryEl = document.querySelector('.gallery');
+
+function makeGaleryElementMarkup(item) {
+    
+    return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+};
+
+const makeGaleryElements = galleryItems.map(makeGaleryElementMarkup).join('');
+let instance;
+divGaleryEl.insertAdjacentHTML('beforeend', makeGaleryElements);
+
+divGaleryEl.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+    window.addEventListener('keydown', onEscapeClose);
+    event.preventDefault();
+    
+    // if (event.target.nodeName !== IMG) {
+    //     return;
+    // }
+    instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`)
+
+instance.show();
+};
+
+// window.addEventListener('keydown', onEscapeClose);
+
+function onEscapeClose(event) {
+    
+    if (event.keyCode === 27) {
+        instance.close();
+     };
+};
+
